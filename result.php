67bc313537087ec5fd32523dc1cd171d98fb5e4d
DanTheBeginner09 <148887741+DanTheBeginner09@users.noreply.github.com>
2024-09-02 01:10:25 +0800

    first commit

---
+++ result.php
@@ -0,0 +1,78 @@
+<?php
+    if(!isset($_SESSION)){
+        session_start();
+    }
+
+if (isset($_SESSION['UserLogin'])) {
+    if ($_SESSION['Access'] == 'Admin') {
+        echo "Welcome Admin " . $_SESSION['UserLogin'];
+    } else {
+        echo "Welcome " . $_SESSION['UserLogin'];
+    }
+} else {
+    echo "Welcome Guest";
+}
+
+include_once("connections/connection.php");
+
+$con = connection();
+
+
+
+ $search =$_GET['search'];
+$sql = "SELECT * FROM student_list  WHERE first_name LIKE '%$search%' || last_name LIKE '%$search%'  ORDER BY id DESC";
+$students = $con->query($sql) or die ($con->error);
+$row = $students->fetch_assoc();
+
+//print_r($row);
+// do{
+
+//     echo $row['first_name']." ".$row['last_name']; 
+
+// }while($row = $students->fetch_assoc());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+<a href="logout.php">Logout</a> <br>
+
+<br><br><a href="add.php">Add New</a> <br>
+    <form action="result.php"  method="get"> 
+    <input type="text" name="search" id="search">
+        <button type="submit" >search</button>
+        
+
+    </form>
+
+<table>
+        
+        <thead>
+        
+        <tr>
+            <th></th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php do{ ?>
+        <tr>
+            <td><a href="details.php?ID=<?php echo $row['id'];  ?>">view</a></td>
+            <td><?php echo $row ['first_name'];?></td>
+            <td><?php echo $row ['last_name']; ?></td>
+        </tr>
+        <?php }while($row = $students->fetch_assoc())?>
+        </tbody>
+    </table>
+</body>
+</html>
